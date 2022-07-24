f11c524eba53b7c5843b9ec16b47daa32c90fda7
Billy Pinkerton <billybattlebones@gmail.com>
2022-07-24 13:54:20 -0700

    Updating Portfolio

---
+++ portfolio/src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+        Heres where my about me will go
+      </p>
+    </div>
+  );
+}
---
+++ portfolio/src/components/pages/Projects.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Blog() {
+  return (
+    <div>
+      <h1>Projects</h1>
+      <p>
+        heres where my projects will go.
+      </p>
+    </div>
+  );
+}
