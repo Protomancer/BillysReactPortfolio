9502bbd6c5f377fe0c8e042ed52a81ff552c5cca
Billy Pinkerton <billybattlebones@gmail.com>
2022-08-10 13:21:24 -0700

    Updating Portfolio

---
+++ build/precache-manifest.bcf7fc425e32ce732731258cb93c998f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "2630925936fc6f6b512d",
+    "url": "/BillysReactPortfolio/static/css/main.4005094a.chunk.css"
+  },
+  {
+    "revision": "2630925936fc6f6b512d",
+    "url": "/BillysReactPortfolio/static/js/main.457d02f0.chunk.js"
+  },
+  {
+    "revision": "04c91ede8f384de91e68",
+    "url": "/BillysReactPortfolio/static/js/runtime~main.d538d29f.js"
+  },
+  {
+    "revision": "f5d94eff6b189c7a5edb",
+    "url": "/BillysReactPortfolio/static/css/2.94362a7a.chunk.css"
+  },
+  {
+    "revision": "f5d94eff6b189c7a5edb",
+    "url": "/BillysReactPortfolio/static/js/2.42a934b5.chunk.js"
+  },
+  {
+    "revision": "b732c88ee927ef5360f2e31b1617ed86",
+    "url": "/BillysReactPortfolio/static/media/noteTaker.b732c88e.png"
+  },
+  {
+    "revision": "61195ab15524ec68b7ddf8b317570ec9",
+    "url": "/BillysReactPortfolio/static/media/github.61195ab1.png"
+  },
+  {
+    "revision": "040a37f9ebfadbd098927dcabc43757a",
+    "url": "/BillysReactPortfolio/static/media/linkedin.040a37f9.png"
+  },
+  {
+    "revision": "491ad1e0c092520859d22aeaf25e205c",
+    "url": "/BillysReactPortfolio/static/media/insta.491ad1e0.png"
+  },
+  {
+    "revision": "01cf5ba1ae991a31a91e701a9bfab510",
+    "url": "/BillysReactPortfolio/static/media/road.01cf5ba1.jpg"
+  },
+  {
+    "revision": "0b96bddb94736337d0b75ba8754e9296",
+    "url": "/BillysReactPortfolio/static/media/DSC_0927.0b96bddb.jpg"
+  },
+  {
+    "revision": "3e456d551425adada1203e81138b1928",
+    "url": "/BillysReactPortfolio/static/media/DSC_0977.3e456d55.jpg"
+  },
+  {
+    "revision": "1461cf906e82bfe91e0943578224339b",
+    "url": "/BillysReactPortfolio/static/media/DSC_0877.1461cf90.jpg"
+  },
+  {
+    "revision": "48d49bb699fc48e56aa04a794c112ddd",
+    "url": "/BillysReactPortfolio/static/media/DSC_0910.48d49bb6.jpg"
+  },
+  {
+    "revision": "e85fefdeb0fdb76aaacc0be755beca51",
+    "url": "/BillysReactPortfolio/index.html"
+  }
+];
